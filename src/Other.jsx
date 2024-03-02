43dc11672a74eb2dbbfc305185d7f8229f38b91b
arpitgoswami <arpitgoswami@hotmail.com>
2024-03-02 13:17:29 +0530

    Initial Message

--- src/Other.jsx
+++ src/Other.jsx
@@ -1,12 +1,14 @@
-import { addDoc, collection } from "@firebase/firestore";
-import { firestore } from "./firebase-config";
-import { useRef } from "react";
+import { addDoc, collection, getDocs } from "@firebase/firestore";
+import { Timestamp } from "@firebase/firestore";
+import { firestore, db } from "./firebase-config";
+import { useRef, useState, useEffect } from "react";
 
 const handleSubmit = (testdata) => {
   const ref = collection(firestore, "users"); // Firebase creates this automatically
 
   let data = {
     Name: testdata,
+    createdAt: Timestamp.fromDate(new Date()),
   };
 
   try {
@@ -25,13 +27,53 @@ function Other() {
     dataRef.current.value = "";
   };
 
+  const refresh = async (e) => {
+    const querySnapshot = await getDocs(collection(db, "collectionName"));
+    const data = querySnapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+    console.log();
+  };
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const fetchedData = await refresh();
+      setData(fetchedData);
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <form onSubmit={submithandler}>
-        <input type="text" ref={dataRef} />
-        <button type="submit">Save</button>
-      </form>
-    </div>
+    <>
+      <div className="App">
+        <form className="m-4 flex space-x-4" onSubmit={submithandler}>
+          <input
+            type="text"
+            placeholder="Name here"
+            class="input input-bordered w-full max-w-xs"
+            ref={dataRef}
+          />
+          <button className="btn btn-primary" type="submit">
+            Save
+          </button>
+          <div>
+            <button className="btn btn-primary">Refresh</button>
+            <div>
+              {data.filter((item) => (
+                <div key={item.id}>
+                  <h2>{item.Name}</h2>
+                  <p>{item.createdAt}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+        </form>
+      </div>
+    </>
   );
 }
 
